9605bb0e40aa9e88743b6b26694b2669ae2d249c
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-04-17 22:32:09 +0200

    removed the comment, noticed an error in my code

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // 🪲 Bug: What's mssing from JS concepts?
         const reactConcepts = new Set(['components', 'jsx', 'hooks', 'async']);
         // 🪲 Bug: Incorrect function call
-        const commonConcepts = findIntersection(jsConcepts, reactConcepts); // changed the second [1] parameter name  
+        const commonConcepts = new Set(jsConcepts, reactConcepts);
         document.getElementById("room2Result").textContent = `The code to unlock the door is: ${Array.from(commonConcepts).join(', ')}`;
     });
 
